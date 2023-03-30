13cd2be46a9e119f7d34588b40b6c2bcc565c067
ahmed-abdat <ahmedeabdat@gmail.com>
2023-03-30 16:18:58 +0000

    updated

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from '@firebase/firestore'
+import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpR9R5L6xuFPuwf7JQkjEnXLGF64qodew",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "mahdara-e8299.firebaseapp.com",
   projectId: "mahdara-e8299",
   storageBucket: "mahdara-e8299.appspot.com",
   messagingSenderId: "7712837976",
   appId: "1:7712837976:web:aace9912481e18d00b152d",
-  measurementId: "G-NFHNGNZH9Z"
+  measurementId: "G-NFHNGNZH9Z",
 };
+
+const apiKey = import.meta.env.VITE_API_KEY;
+
+console.log(apiKey);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
-export const auth = getAuth(app)
+export const db = getFirestore(app);
+export const auth = getAuth(app);
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv';
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
